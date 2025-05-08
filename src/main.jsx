c8d77423950c53aabc14b57b959e34dcdd44af32
Seona Cha <seona.cha@concentrix.com>
2025-05-08 17:00:42 +0900

    스타일 수정
    - 일부 마크업 변경
    - sass 모듈 설치
    - style 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './assets/scss/index.scss'
+import './assets/scss/app.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
